5d295283221ad80a833d578c142138d39116a833
rubenmoya <rmoyarodriguez@gmail.com>
2015-11-19 15:32:15 +0100

    Code cleanup.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,81 +1,55 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     @project = Project.find(params[:project_id])
     @tasks = @project.tasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
   end
 
-  # GET /tasks/new
   def new
     @project = Project.find(params[:project_id])
     @task = @project.tasks.new
     @users = User.all
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
     @project = Project.find(params[:project_id])
     @task = @project.tasks.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to project_task_path(@project, @task), notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to project_task_path(@project, @task), flash: { :success => "Task has been created successfully" }
+    else
+      render :new, flash: { :danger => "Task has not been created" }
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to project_task_path(@project, @task), notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to project_task_path(@project, @task), flash: { :success => "Task has been updated successfully" }
+    else
+      render :edit, flash: { :danger => "Task has not been updated" }
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
-
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to project_tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to project_tasks_url, flash: { :success => "Task has been deleted successfully" }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @project = Project.find(params[:project_id])
-      @task = @project.tasks.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:name, :project_id, assignments_attributes: [:id, :user_id, :hours, :_destroy])
-    end
+  def set_task
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :project_id, assignments_attributes: [:id, :user_id, :hours, :_destroy])
+  end
 end
