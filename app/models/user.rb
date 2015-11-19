2454241daa740e1b31a7e5bd415223ef52b503f0
rubenmoya <rmoyarodriguez@gmail.com>
2015-11-19 14:18:59 +0100

    Fix user collection in task form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def self.not_in_task task, uid
+    where("id not in (?)", task.assignments.collect{|e| e.user_id }.select{|i| i != uid})
+  end
 end
